4a0e1c24cd245ff11a9d13133144711f3c3cc407
moitrasa <moitrasa@amazon.com>
2023-07-21 18:21:57 +0530

    Code push to repo

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# aws-sagemaker-llama2-chroma
+```markdown
+# PDF Search Engine with Streamlit, ChromaDB, and AWS SageMaker using jumpstart-dft-meta-textgeneration-llama-2-7b-f
+
+This project is a simple search engine for PDF files. It uses Streamlit for the web interface, ChromaDB for storing and querying document embeddings, and AWS SageMaker for generating responses to user queries.
+
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- You have an AWS account.
+- You have installed the required Python packages listed in the `requirements.txt` file.
+
+## AWS Setup
+
+Before running the code, you need to set up an AWS SageMaker domain and user profile. Follow these steps:
+
+1. Log in to your AWS account and navigate to Amazon SageMaker.
+2. Create a new domain and user profile.
+3. Navigate to the JumpStart model section and launch the `jumpstart-dft-meta-textgeneration-llama-2-7b-f` model.
+4. Wait for the endpoint to be deployed. This may take a few minutes.
+
+Once the endpoint is deployed, you can proceed with the next steps.
+
+
+## Features
+
+- Upload a PDF file and extract the text from each page.
+- Store the text and its embeddings in ChromaDB.
+- Enter a query and get the most relevant page from the PDF.
+- Use AWS SageMaker to generate a response to the query.
+
+## Installation
+
+1. Clone this repository.
+2. Install the required Python libraries using pip:
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Run the Streamlit app:
+
+```bash
+streamlit run app.py
+```
+
+## Usage
+
+1. Upload a PDF file.
+2. Enter a query related to the content of the PDF.
+3. Click "Submit Query" to get the most relevant response.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project uses the following license: [MIT No Attribution](LICENSE).
---
+++ app.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pdfplumber
+from sentence_transformers import SentenceTransformer
+from chromadb import Client
+import boto3
+import json
+import nltk
+import numpy as np
+from nltk.tokenize import sent_tokenize
+import time
+
+# Download the Punkt tokenizer
+nltk.download('punkt')
+
+# Initialize clients
+chroma_client = Client()
+endpoint_name = "jumpstart-dft-meta-textgeneration-llama-2-7b-f"
+
+# SentenceTransformers model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Generate embeddings
+@st.cache_resource
+def get_embeddings(text):
+  return model.encode(sent_tokenize(text)).tolist() 
+
+# Upload page  
+def upload_page(collection, file_id, page_num, page_text):
+  embeddings = get_embeddings(page_text)
+  page_embedding = np.mean(embeddings, axis=0).tolist()
+
+  collection.add(
+    documents=[page_text],
+    metadatas=[{"page": page_num}],
+    ids=[f"{file_id}_page{page_num}"],
+    embeddings=[page_embedding]
+  )
+
+# Query endpoint
+def query_endpoint(payload):
+    client = boto3.client("sagemaker-runtime")
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType="application/json",
+        Body=json.dumps(payload),
+        CustomAttributes="accept_eula=true",
+    )
+    response = response["Body"].read().decode("utf8")
+    response = json.loads(response)
+    return response
+
+# Streamlit UI and app flow
+def main():
+
+  # Check if the collection exists, if not create it
+  if 'pdf_pages' not in chroma_client.list_collections():
+    try:
+      chroma_client.create_collection('pdf_pages')
+    except ValueError as e:
+      if "already exists" in str(e):
+        pass  # Collection already exists, no need to create it
+      else:
+        raise  # Re-raise any other ValueError
+
+  collection = chroma_client.get_collection('pdf_pages')
+
+  uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+  if uploaded_file is not None:
+    file_id = str(time.time())  # Unique identifier for each file
+    with pdfplumber.open(uploaded_file) as pdf:
+      for i, page in enumerate(pdf.pages):
+        page_text = page.extract_text()
+        upload_page(collection, file_id, i+1, page_text)
+
+  query = st.text_input("Enter your query")
+  if st.button("Submit Query"):
+    # Query ChromaDB for relevant documents
+    embeddings = get_embeddings(query)
+    results = collection.query(query_texts=[query], n_results=1)
+    
+    if results['documents']:
+        top_result_text = results['documents'][0][0]
+
+        # Use the top result text as input to the model
+        dialog = [
+            {"role": "system", "content": "You are chatting with an AI assistant."},
+            {"role": "user", "content": top_result_text}
+        ]
+        payload = {
+            "inputs": [dialog], 
+            "parameters": {"max_new_tokens": 500, "top_p": 0.9, "temperature": 0.6}
+        }
+        result = query_endpoint(payload)[0]
+        st.write(f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}")
+        st.write("\n==================================\n")
+    else:
+        st.write("No matching documents found for the query.")
+
+if __name__ == "__main__":
+    main()
